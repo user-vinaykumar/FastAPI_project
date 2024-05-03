b2e07d88fd9e3ba3843675f68c219d5f277db223
user_vk_ <rossilorenzo81773@gmail.com>
2024-05-03 13:47:36 +0530

    get post of http is created and understood correctly.

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
 @app.get('/')
 def root():
-    return {'vinay' : 'vinay.kumar@company.com'}
+    return {'message' : 'Welcome to my API'}
 
 @app.get('/posts')
 def get_posts():
-    return {'data' : 'this is my post'}
+
+    return {'data' : 'This is your posts'}
+
+# @app.post('/createposts')
+# def create_posts(payload: dict = Body(...)):
+#     print(payload)
+#     return {"new_post" : f"title :- {payload['title']} and "
+#                          f"content :- {payload['content']}"}
+
